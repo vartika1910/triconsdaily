b6dcbc4d4244ebce064dbb9f31b9a7ab59fd8fa3
Mukul Kumar Sahu <MukulKumar.Sahu@triconinfotech.com>
2023-11-09 17:08:40 +0530

    Added Header

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { ReactDOM } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { Navbar } from "./frontend/components/navbar";
 
 
 const App = () =>{
     return(
         <>
-        
+        <Navbar/>
         </>
     )
 }
---
+++ frontend/components/navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import { Link } from "react-dom/client";
+
+export const Navbar = () => {
+    return (
+
+        <>
+            <p>abcd</p>
+            <header>
+                <nav>
+                    <ul >
+
+                        <li><a href="#h">Home</a></li>
+                        <li><a href="#Search" >Search</a></li>
+                        <li><a href="#Cart" >Cart</a></li>
+                        <li><a href="#about" >About Us</a></li>
+                        <li><a href="#Logout" >Logout</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
+    );
+}
